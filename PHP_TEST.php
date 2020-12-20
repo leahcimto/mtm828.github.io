50d5d80c9356fb1d6a5945aaa202dc22722c0cd4
MTM828 <71281115+MTM828@users.noreply.github.com>
2020-12-20 17:14:11 +0300

    Create PHP_TEST.php

---
+++ PHP_TEST.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>MTM828 - Home</title>
+        <link rel="icon" href="https://avatars2.githubusercontent.com/u/71281115?s=460&v=4">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <mete name="description" content="MTM828">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abel|Alata">
+        <link rel="stylesheet" href="https://mtm828.github.io/RECOURCES/CSS.css">
+
+        <body>
+            <a href="https://github.com/mtm828"><img style="margin-top: 10px; margin-left: auto; margin-right: auto; display: block;" width="75px" height="75px" src="https://avatars2.githubusercontent.com/u/71281115?s=460&v=4"></a>
+            <h1 style="text-align: center; font-size: 200%; border-bottom: none; font-family: abel;">MTM828</h1>
+            <ul class="topNav" id="topNav">
+                <li><a href="">Link A</a></li>
+                <li><a href="">Link B</a></li>
+                <li style="float: right;">
+                    <a>
+                        <label class="switch">
+                            <input type="checkbox" id="darkModeSwitch">
+                            <span class="slider" onclick="toggleDarkMode()"></span>
+                        </label>
+                    </a>
+                </li>
+            </ul>
+            <?php
+                $CRAZY = "MTM828";
+                echo "<p>$CRAZY has officially gone CRAZY!</p>";
+            ?>
+            <div class="topNavbarWrapper"></div>
+            <div class="pageContent">
+                <ul class="footer">
+                    <li><a href="https://github.com/mtm828"><img width="50px" height="50px" src="https://avatars2.githubusercontent.com/u/71281115?s=460&v=4"></a></li>
+                    <li><a href="https://mtm828.github.io/index.html">Home</a></li>
+                </ul>
+            </div>
+        </body>
+    
+        <script src="https://mtm828.github.io/RECOURCES/JS.js">
+        </script>
+    </head>
+</html>
