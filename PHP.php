23e2d3f120a0f56631919268c1271892003ce04a
MTM828 <71281115+MTM828@users.noreply.github.com>
2020-12-20 20:45:50 +0300

    $CRAZY = "MTM828";

---
+++ PHP.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <body>
+        <?php
+            $CRAZY = "MTM828";
+            echo "<p>$CRAZY has officially gone CRAZY!</p>";
+        ?>
+    </body>
+</html>
