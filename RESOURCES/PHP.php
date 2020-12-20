238a5208d49fc76a56b6f879b46917207bf4bfc9
MTM828 <71281115+MTM828@users.noreply.github.com>
2020-12-20 21:59:33 +0300

    (ノಠ益ಠ)ノ

--- RESOURCES/PHP.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-    <body>
-        <?php
-            $CRAZY = "MTM828";
-            echo "<p>$CRAZY has officially gone CRAZY!</p>";
-        ?>
-    </body>
-</html>
