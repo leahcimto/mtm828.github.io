56986eea24bb39ae2576d8c9cbfba80da0724a21
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-01-02 15:09:55 +0300

    Update JS.js

--- RECOURCES/JS.js
+++ RECOURCES/JS.js
@@ -19,5 +19,5 @@ if (localStorage.getItem("darkMode") == undefined) {
 if (localStorage.getItem("darkMode") == "true") {
     document.getElementById("darkModeSwitch").checked = true;
     localStorage.setItem("darkMode", "true");
-    toggleDarkMode();
+    //toggleDarkMode();
 }
