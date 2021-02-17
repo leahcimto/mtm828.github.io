a4e9697429da5e69ee06fcacb448c2bef129717b
MTM828 <71281115+MTM828@users.noreply.github.com>
2021-02-17 13:23:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 var i;
 var j;
-
+var darkmode;
 function toggleDarkMode() {
-    var switchState = document.getElementById("darkModeSwitch").checked;
     var elements = document.querySelectorAll("*");
     for (i=0; i<elements.length; i++) {
-        if (switchState) {elements[i].classList.add("darkMode");} else {elements[i].classList.remove("darkMode");}
+        if (darkMode) {elements[i].classList.add("darkMode");} else {elements[i].classList.remove("darkMode");}
     }
-    if (switchState) {
+    if (darkMode) {
         window.localStorage.setItem("darkMode", "true");
     } else {
         window.localStorage.setItem("darkMode", "false");
@@ -18,8 +17,8 @@ window.onload = function() {
     window.setInterval(toggleDarkMode, 100);
     if (window.localStorage == null) {window.localStorage.setItem("darkMode", "false");}
     if (window.localStorage.getItem("darkMode") == "true") {
-        document.getElementById("darkModeSwitch").checked = true;
+        darkMode = true;
     } else {
-        document.getElementById("darkModeSwitch").checked = false;
+        darkMode = false;
     }
 }
